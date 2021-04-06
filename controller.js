394f73caf5bdf9b3d0a7d20e6079f933fc280b9f
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-04-06 11:58:14 +0800

    Sample deploy heruko

---
+++ controller.js
@@ -0,0 +1,49 @@
+
+var date = new Date()
+
+function connectFunc(){
+    
+    document.getElementById('status').value = "";
+    document.getElementById('status').value += "Connecting ....";
+    
+    client = mqtt.connect(document.getElementById('broker').value)
+
+    client.on("connect", function(){
+        document.getElementById('status').value = "";
+        document.getElementById('status').value += "Connected!";
+    })
+    
+    client.on("message", function (topic, payload) {
+      if (topic == document.getElementById('sub-topic').value){
+        document.getElementById('incomingMessage').innerHTML += `<tr><td>${topic}</td><td>${payload}</td><td>${date.toDateString()+" "+ date.toLocaleTimeString()}</td></tr>`
+      }
+    })
+}
+
+function publishFunc() {
+    if (document.getElementById('pub-topic').value != "" && document.getElementById('pub-payload').value != "") {
+        client.publish(document.getElementById('pub-topic').value, document.getElementById('pub-payload').value)
+        document.getElementById('displayPubMessage').innerHTML += `<tr><td>${document.getElementById('pub-topic').value}</td><td>${document.getElementById('pub-payload').value}</td><td>${date.toDateString()+" "+ date.toLocaleTimeString()} </td></tr>`
+        
+    } else {
+        alert("Please fill in the topic and payload! ")
+    }
+}
+
+function subscribeFunc(){
+    
+    if (document.getElementById('sub-topic').value != "") {
+        client.subscribe(document.getElementById('sub-topic').value, function (err) {
+            if (err) {
+                console.log("Error in subscribing topic!")
+            }
+        document.getElementById('displaySubMessage').innerHTML  += `<tr><td>${document.getElementById('sub-topic').value }</td><td>${date.toDateString()+" "+ date.toLocaleTimeString()}</td></tr>`
+        })
+    } else {
+        alert("Please fill in the topic! ")
+    }
+}
+
+
+
+  
